02cc5842bb1a3b595d1cdcc6579301be2b785e80
Jacob West <jacobthewest@gmail.com>
2019-04-12 23:36:30 -0600

    deleted a now useless folder

--- Datalog Interpreter_Lab_4/Datalog_Interpreter/Datalog_Interpreter/Parameter.cpp
+++
@@ -1,6 +0,0 @@
-#include "Parameter.h"
-
-Parameter::Parameter(string type, string value) {
-	this->type_m = type; //Needed for later calculations, even though it won't be returned.
-	this->value_m = value; //To be returned in the toString function
-}
